480e78606d0c95914293904a9c0f979fbdac2117
Yuanxingqian <844800670@qq.com>
2018-08-06 17:18:43 +0800

    增加了新的模板
    创建增加了一个新的模板

---
+++ config.php
@@ -0,0 +1,52 @@
+<?php return array (
+  'name' => '东亚新闻',
+  'author' => 'Yuan',
+  'dirname' => 'dynews_temp',
+  'homepage' => 'http://www.phpcms.cn/',
+  'version' => '1.0',
+  'disable' => 0,
+  'file_explan' => 
+  array (
+    'templates|dynews_temp' => 
+    array (
+      'category.html' => '文章频道页',
+      'category_download.html' => '下载频道页',
+      'category_picture.html' => '图片频道页',
+      'download.html' => '下载连接页',
+      'footer.html' => '底部',
+      'header.html' => '顶部',
+      'header_min.html' => '迷你顶',
+      'header_page.html' => '单网页头部',
+      'index.html' => '网站首页',
+      'list.html' => '列表页',
+      'list_download.html' => '下载列表页',
+      'list_picture.html' => '图片列表页',
+      'message.html' => '消息提示页',
+      'page.html' => '单网页',
+      'rss.html' => 'RSS页',
+      'search.html' => '搜索',
+      'show.html' => '文章内容页',
+      'show_download.html' => '下载内容页',
+      'show_picture.html' => '图片内容页',
+      'tag.html' => 'tag',
+    ),
+    'templates|dynews_temp|' => 
+    array (
+      'announce' => '公告',
+      'comment' => '评论',
+      'content' => '内容模型',
+      'formguide' => '表单向导',
+      'link' => '友情链接',
+      'member' => '会员中心',
+      'message' => '短消息',
+      'mood' => '新闻心情',
+      'pay' => '支付',
+      'poster' => '广告',
+      'search' => '搜索',
+      'special' => '专题',
+      'vote' => '投票',
+      'wap' => '手机门户',
+      'yp' => '黄页模板',
+    ),
+  )
+);?>
